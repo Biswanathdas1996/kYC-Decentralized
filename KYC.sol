913df6f9dfcf400b11b15e8d64be9c5c7a0cb9b9
your_username <your_email_address@example.com>
2022-06-01 20:23:22 +0530

    ok

--- KYC.sol
+++ KYC.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-    struct User {
+    struct UserKYC {
         string abiLink;
         string data;
         string uid;
@@ -9,20 +9,21 @@ pragma solidity ^0.8.13;
     }
 
 interface  InterfaceKYC{
+    function getUserData(string memory uid) external view returns(UserKYC memory);
     function getAllUser() external view returns(string[] memory);
-    function getUserData(string memory uid) external view returns(User memory);
-    function addUser(string memory uid, string memory abiLink , string memory data ) external ;
+    
+    function addUser(string memory uid, string memory abiLink , string memory data ) external;
     function upadteUserAbi(string memory uid ,string memory abiLink, string memory data  ) external;
     function setUserMetadata(string memory metadata, string memory uid) external;
 }
 
 contract KYC {
 
-     mapping(string => User) public users;
+     mapping(string => UserKYC) public users;
      string[] public userList ;
 
-     function addUser(string memory uid, string memory abiLink , string memory data ) external virtual {
-        User memory newUser = User({
+     function addUser(string memory uid, string memory abiLink , string memory data ) external  {
+        UserKYC memory newUser = UserKYC({
             abiLink:abiLink,
             data: data,
             uid:uid,
@@ -36,19 +37,19 @@ contract KYC {
         return userList;
     }
 
-    function getUserData(string memory uid) external view returns(User memory){
-        User storage userData = users[uid];
+    function getUserData(string memory uid) external view returns(UserKYC memory){
+        UserKYC storage userData = users[uid];
         return userData;
     }
 
     function upadteUserAbi(string memory uid ,string memory abiLink, string memory data  ) external {
-         User storage userData = users[uid];
+         UserKYC storage userData = users[uid];
          userData.abiLink = abiLink;
          userData.data = data;
     }
 
     function setUserMetadata(string memory metadata, string memory uid) external {
-         User storage userData = users[uid];
+         UserKYC storage userData = users[uid];
          userData.metadata = metadata;
     }
 
