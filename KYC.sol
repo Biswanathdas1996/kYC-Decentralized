c8a1741f79d98915327cd8e6e90f2ff0577ecf9d
your_username <your_email_address@example.com>
2022-06-08 12:58:36 +0530

    ok

--- KYC.sol
+++ KYC.sol
@@ -9,13 +9,8 @@ pragma solidity ^0.8.13;
     }
 
 interface  InterfaceKYC{
-    struct UserKYC {
-        string abiLink;
-        string data;
-        string uid;
-        string metadata;
-    }
-    function getUserData(string memory uid) external view returns(UserKYC memory);
+    function getUserData(string memory uid) external view  returns(string[] memory);
+    function validateUser(string memory uid) external view  returns(bool);
     function getAllUserKYCData() external view returns(string[] memory);
     function addUser(string memory uid, string memory abiLink , string memory data ) external;
     function upadteUserAbi(string memory uid ,string memory abiLink, string memory data  ) external;
@@ -42,9 +37,24 @@ contract KYC {
         return userList;
     }
 
-    function getUserData(string memory uid) external view returns(UserKYC memory){
+    function getUserData(string memory uid) external view  returns(string[] memory){
         UserKYC storage userData = users[uid];
-        return userData;
+       string[] memory returnData = new string[](4); // 5 empty items
+        returnData[0] = userData.uid;
+        returnData[1] = userData.abiLink;
+        returnData[2] = userData.metadata;
+        returnData[3] = userData.data;
+       return returnData;
+    }
+
+
+    function validateUser(string memory uid) external view  returns(bool){
+        bytes memory tempEmptyStringTest = bytes(users[uid].uid);
+        if(tempEmptyStringTest.length == 0){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     function upadteUserAbi(string memory uid ,string memory abiLink, string memory data  ) external {
